cc8ccba1119a358576cf8516ceeba305ecbe6f7f
Matt Venn <matt@mattvenn.net>
2021-01-27 12:29:22 +0100

    power pins

--- wrapper.v
+++ wrapper.v
@@ -3,6 +3,16 @@
     `define MPRJ_IO_PADS 38    
 `endif
 module wrapper (
+`ifdef USE_POWER_PINS
+    inout vdda1,	// User area 1 3.3V supply
+    inout vdda2,	// User area 2 3.3V supply
+    inout vssa1,	// User area 1 analog ground
+    inout vssa2,	// User area 2 analog ground
+    inout vccd1,	// User area 1 1.8V supply
+    inout vccd2,	// User area 2 1.8v supply
+    inout vssd1,	// User area 1 digital ground
+    inout vssd2,	// User area 2 digital ground
+`endif
     // interface as user_proj_example.v
     input wire wb_clk_i,
     input wire wb_rst_i,
