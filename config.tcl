5724f8e97ec5443338b046d7e5eeac7ab2449a10
Matt Venn <matt@mattvenn.net>
2021-04-27 18:18:07 +0200

    config for rc0.12

--- config.tcl
+++ config.tcl
@@ -1,4 +1,6 @@
 # User config
+set script_dir [file dirname [file normalize [info script]]]
+
 set ::env(DESIGN_NAME) wrapper
 
 # Change if needed
@@ -21,9 +23,10 @@ set ::env(CLOCK_PORT) "wb_clk_i"
 set ::env(DESIGN_IS_CORE) 0
 set ::env(GLB_RT_MAXLAYER) 5
 
+set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
+set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 
-set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
-if { [file exists $filename] == 1} {
-	source $filename
-}
+set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
+
+set ::env(RUN_CVC) 0
 
