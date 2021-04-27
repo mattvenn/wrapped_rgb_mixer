32be5fdd65de9c1be64f8d0545bd1192d64cf371
Matt Venn <matt@mattvenn.net>
2021-04-27 17:56:34 +0200

    update to new style of assignment

--- wrapper.v
+++ wrapper.v
@@ -2,7 +2,7 @@
 `ifdef FORMAL
     `define MPRJ_IO_PADS 38    
 `endif
-module wrapper (
+module wrapped_rgb_mixer (
 `ifdef USE_POWER_PINS
     inout vdda1,	// User area 1 3.3V supply
     inout vdda2,	// User area 2 3.3V supply
@@ -52,20 +52,21 @@ module wrapper (
     assign wbs_ack_o    = active ? buf_wbs_ack_o    : 1'b0;
     assign wbs_dat_o    = active ? buf_wbs_dat_o    : 32'b0;
     assign la_data_out  = active ? buf_la_data_out  : 32'b0;
-    assign io_out       = active ? buf_io_out       : `MPRJ_IO_PADS'b0;
-    assign io_oeb       = active ? buf_io_oeb       : `MPRJ_IO_PADS'b0;
+    assign io_out       = active ? buf_io_out       : {`MPRJ_IO_PADS{1'b0}};
+    assign io_oeb       = active ? buf_io_oeb       : {`MPRJ_IO_PADS{1'b0}};
     `include "properties.v"
     `else
     // tristate buffers
     assign wbs_ack_o    = active ? buf_wbs_ack_o    : 1'bz;
     assign wbs_dat_o    = active ? buf_wbs_dat_o    : 32'bz;
     assign la_data_out  = active ? buf_la_data_out  : 32'bz;
-    assign io_out       = active ? buf_io_out       : `MPRJ_IO_PADS'bz;
-    assign io_oeb       = active ? buf_io_oeb       : `MPRJ_IO_PADS'bz;
+    assign io_out       = active ? buf_io_out       : {`MPRJ_IO_PADS{1'bz}};
+    assign io_oeb       = active ? buf_io_oeb       : {`MPRJ_IO_PADS{1'bz}};
     `endif
 
     // permanently set oeb so that outputs are always enabled: 0 is output, 1 is high-impedance
-    assign buf_io_oeb = `MPRJ_IO_PADS'h0;
+    assign buf_io_oeb = {`MPRJ_IO_PADS{1'b0}};
+
     // instantiate your module here, connecting what you need of the above signals
     rgb_mixer rgb_mixer0(
         .clk        (wb_clk_i),
