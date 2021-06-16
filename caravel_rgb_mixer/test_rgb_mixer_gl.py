f83c2cce35f29ee75ba13d1a02b2553bfd498399
Matt Venn <matt@mattvenn.net>
2021-06-16 17:28:28 +0200

    GL test support

---
+++ caravel_rgb_mixer/test_rgb_mixer_gl.py
@@ -0,0 +1,73 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, with_timeout, Timer
+import random
+from test_encoder import Encoder
+
+clocks_per_phase = 10
+
+async def run_encoder_test(encoder, max_count):
+    for i in range(clocks_per_phase * 2 * max_count):
+        await encoder.update(1)
+
+    # let noisy transition finish, otherwise can get an extra count
+    for i in range(10):
+        await encoder.update(0)
+
+# takes ~???  seconds on my PC
+@cocotb.test()
+async def test_rgb_mixer_gl(dut):
+    clock = Clock(dut.clk, 25, units="ns")
+    cocotb.fork(clock.start())
+    
+    encoder0 = Encoder(dut.clk, dut.enc0_a, dut.enc0_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
+    encoder1 = Encoder(dut.clk, dut.enc1_a, dut.enc1_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
+    encoder2 = Encoder(dut.clk, dut.enc2_a, dut.enc2_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
+
+    # have to get the outputs non z
+    await encoder0.update(0)
+    await encoder1.update(0)
+    await encoder2.update(0)
+
+    dut.RSTB <= 0
+    dut.power1 <= 0;
+    dut.power2 <= 0;
+    dut.power3 <= 0;
+    dut.power4 <= 0;
+
+    await ClockCycles(dut.clk, 8)
+    dut.power1 <= 1;
+    await ClockCycles(dut.clk, 8)
+    dut.power2 <= 1;
+    await ClockCycles(dut.clk, 8)
+    dut.power3 <= 1;
+    await ClockCycles(dut.clk, 8)
+    dut.power4 <= 1;
+
+    await ClockCycles(dut.clk, 80)
+    dut.RSTB <= 1
+
+    # wait for firmware to load and design become active - could do this better by writing to mprj io and await that
+    await Timer(180, 'us')
+
+    # pwm should all be low at start
+    assert dut.pwm0_out == 0
+    assert dut.pwm1_out == 0
+    assert dut.pwm1_out == 0
+
+    # do 3 ramps for each encoder 
+    max_count = 255
+    await run_encoder_test(encoder0, max_count)
+    await run_encoder_test(encoder1, max_count)
+    await run_encoder_test(encoder2, max_count)
+
+    # sync to pwm
+    await RisingEdge(dut.pwm0_out)
+    await ClockCycles(dut.clk, 1)
+
+    # pwm should all be on for max_count 
+    for i in range(max_count): 
+        assert dut.pwm0_out == 1
+        assert dut.pwm1_out == 1
+        assert dut.pwm2_out == 1
+        await ClockCycles(dut.clk, 1)
