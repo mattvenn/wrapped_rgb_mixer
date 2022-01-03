7eefa238bd28ffd814cdbcb0bf6d487d96b80456
Matt Venn <matt@mattvenn.net>
2022-01-03 13:37:59 +0100

    wip

--- .github/scripts/install_oss_suite.sh
+++ .github/scripts/install_oss_suite.sh
@@ -1,2 +1,2 @@
-curl https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2021-09-29/oss-cad-suite-linux-x64-20210929.tgz 
-tar -xzf oss-cad-suite-linux-x64-20210929.tgz 
+curl -L https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2021-09-29/oss-cad-suite-linux-x64-20210929.tgz --output oss.tgz
+tar -xzf oss.tgz 
--- .github/scripts/install_riscv_gcc.sh
+++ .github/scripts/install_riscv_gcc.sh
@@ -1,2 +1,2 @@
-curl https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14.tar.gz 
-tar -xzf riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14.tar.gz
+curl -L https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14.tar.gz  --output riscv.tgz
+tar -xzf riscv.tgz
