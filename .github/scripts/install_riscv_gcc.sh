a141ea3f588854a6a800223539330499ed623db5
Matt Venn <matt@mattvenn.net>
2022-01-03 13:24:57 +0100

    wip

---
+++ .github/scripts/install_riscv_gcc.sh
@@ -0,0 +1,2 @@
+curl https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14.tar.gz 
+tar -xzf riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14.tar.gz
